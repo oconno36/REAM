0c62eee18b83488c3e58d2b9029e0ae4d46446a9
oconno36 <54715887+oconno36@users.noreply.github.com>
2020-11-11 11:26:17 -0500

    Update MovieAppInterface.java

--- MovieAppInterface.java
+++ MovieAppInterface.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,12 +8,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -32,15 +34,15 @@ public class MovieAppInterface extends JFrame implements ActionListener
 	private JPanel panel;
 	JLabel prompt = new JLabel();
 	JTextArea movies = new JTextArea();
-	JTextArea info = new JTextArea();
-	JTextField search = new JTextField(15);
+	JTextField search = new JTextField(20);
+	JLabel movie1 = new JLabel();
+	JLabel movie2 = new JLabel();
 	private JButton searchButton;
 	// images
 	JLabel label;
 	JLabel label2;
 	boolean moviePicUp = false;
-	static ArrayList<String> movieArray = new ArrayList();
-	static ArrayList<String> movieSearch = new ArrayList();
+	static ArrayList<Movie> movieArray = new ArrayList();
 	/**
 	 * constructor creates all components of a ColorFrame! 
 	 */
@@ -66,14 +68,15 @@ public class MovieAppInterface extends JFrame implements ActionListener
 		gBut8.addActionListener(this);
 		gBut9.addActionListener(this);
 		searchButton.addActionListener(this);
-		panel= new JPanel();
+		panel = new JPanel();
+		
 		panel.add(but1);
 		panel.add(but2);
 		panel.add(but3);
 
 		JFrame frame = new JFrame();
 		frame.add(panel);
-		frame.setSize(300, 400);
+		frame.setSize(350, 400);
 		frame.setTitle("MovieApp");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -85,36 +88,6 @@ public class MovieAppInterface extends JFrame implements ActionListener
 	@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent event) 
 	{ 
-		if(event.getActionCommand().equals("Profile"))
-		{
-			Scanner obj = null;
-			File users = null;
-	
-			try 
-			{
-			users=new File("users.txt");
-			obj = new Scanner(users);
-			String s = "";
-		
-			while(obj.hasNextLine()) 
-			{
-				s = obj.nextLine() + "\n";
-			}
-			int i= s.indexOf(", ");
-			String start= s.substring(0,i);
-			String end=s.substring(i+2,s.length());
-			info.setText("Username: " + start + "\nPassword: " + end);
-    			}
-			catch(Exception e)
-			{
-				System.out.println(e);
-			}
-			finally
-			{
-				obj.close();
-			}
-			panel.add(info);
-		}
 		if(event.getActionCommand().equals("Movies")) {
 			// Removes Genres buttons
 			panel.remove(gBut4);
@@ -123,29 +96,37 @@ public class MovieAppInterface extends JFrame implements ActionListener
 			panel.remove(gBut7);
 			panel.remove(gBut8);
 			panel.remove(gBut9);
-			panel.remove(info);
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			
+			// Search feature
 			panel.add(search);
 			panel.add(searchButton);
 			
 			// Movie list
+			/*
 			panel.add(prompt);
 			prompt.setText("This is a list of movies: ");
 			movies.setText("\t \t \t");
+			
 			for(int i = 0; i < movieArray.size(); i++) {
 				movies.setText(movies.getText() + "\n" + movieArray.get(i));
 			}
 			panel.add(movies);
+			*/
+			
+			// Movie Labels with pictures
+			//panel.setLayout(new GridLayout(14, 2));
 			
 			try {
 				if(!moviePicUp) {
-					BufferedImage image = ImageIO.read(new File("./LordOfTheRings.png"));
-					label = new JLabel(new ImageIcon(image));
-					panel.add(label);
-
-					BufferedImage image2 = ImageIO.read(new File("./HarryPotter.png"));
-					label2 = new JLabel(new ImageIcon(image2));
-					panel.add(label2);
-
+					for(int i = 0; i < movieArray.size(); i++) {
+						BufferedImage image = ImageIO.read(movieArray.get(i).getImageFile());
+						Image scaledImage = image.getScaledInstance(50, 75, Image.SCALE_SMOOTH);
+						movieArray.get(i).movieImage = new JLabel(new ImageIcon(scaledImage));
+					}
 					moviePicUp = true;
 				}
 			} 
@@ -154,9 +135,27 @@ public class MovieAppInterface extends JFrame implements ActionListener
 				e.printStackTrace();
 			}
 			
+			for(int i = 0; i < movieArray.size(); i++) {
+				movieArray.get(i).getMovieTitle().setText(movieArray.get(i).getName());
+				panel.add(movieArray.get(i).getMovieTitle());
+				panel.add(movieArray.get(i).getMovieImage());
+			}
 		}
 		
 		if(event.getActionCommand().equals("Search")) {
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getName().contains(search.getText())) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			
+			/*
 			movies.setText("");
 			for(int i = 0; i < movieArray.size(); i++) {
 				if(movieArray.get(i).contains(search.getText())) {
@@ -164,15 +163,20 @@ public class MovieAppInterface extends JFrame implements ActionListener
 				}
 			}
 			movies.setText(movies.getText() + "\n\t\t\t");
+			*/
 		}
 		
 		if(event.getActionCommand().equals("Genres")) {
+			// Remove Movie features
 			movies.setText("");
 			prompt.setText("This is a list of genres\n");
-			panel.remove(label);
-			panel.remove(label2);
-			panel.remove(info);
+			panel.remove(search);
+			panel.remove(searchButton);
 			moviePicUp = false;
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
 			
 			panel.add(prompt);
 			panel.add(gBut4);
@@ -184,27 +188,88 @@ public class MovieAppInterface extends JFrame implements ActionListener
 		}
 		
 		if(event.getActionCommand().equals("Fantasy")) {
-			movies.setText("Lord of the Ring \t \t \nHarry Potter");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Fantasy")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("Lord of the Ring \t \t \nHarry Potter");
+			//movies.setText(movies.getText() + "\nMaleficent \nThe Hobbit");
 		}
 		
 		if(event.getActionCommand().equals("Comedy")) {
-			movies.setText("A Goofy Movie \t \t \nMegamind");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Comedy")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("A Goofy Movie \t \t \nMegamind");
 		}
 		
 		if(event.getActionCommand().equals("Romance")) {
-			movies.setText("The Pricess Bride \t \t \nCinderella");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Romance")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("The Pricess Bride \t \t \nCinderella");
 		}
 		
 		if(event.getActionCommand().equals("Sci-Fi")) {
-			movies.setText("The Martian \t \t \nEnder's Game");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Sci-Fi")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("The Martian \t \t \nEnder's Game");
 		}
 		
 		if(event.getActionCommand().equals("Mystery")) {
-			movies.setText("The Hound of the Baskerville \t \t \nA Study in Scarlet");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Mystery")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("The Hound of the Baskerville \t \t \nA Study in Scarlet");
 		}
 		
 		if(event.getActionCommand().equals("Feel-Good")) {
-			movies.setText("Zootopia \t \t \nRatatouille");
+			for(int i = 0; i < movieArray.size(); i++) {
+				panel.remove(movieArray.get(i).getMovieTitle());
+				panel.remove(movieArray.get(i).getMovieImage());
+			}
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Feel-Good")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+			//movies.setText("Zootopia \t \t \nRatatouille");
 		}
 		
 		//
@@ -217,20 +282,34 @@ public class MovieAppInterface extends JFrame implements ActionListener
 	public static void main(String[] args)
 	{
 		MovieAppInterface color = new MovieAppInterface();
-		movieArray.add("Maleficent");
-		movieArray.add("The Hobbit");
-		movieArray.add("Lord of the Rings");
-		movieArray.add("Harry Potter");
-		movieArray.add("The Martian");
-		movieArray.add("Ender's Game");
-		movieArray.add("The Pricess Bride");
-		movieArray.add("A Goofy Movie");
-		movieArray.add("Megamind");
-		movieArray.add("Cinderella");
-		movieArray.add("The Hound of the Baskerville");
-		movieArray.add("A Study in Scarlet");
-		movieArray.add("Zootopia");
-		movieArray.add("Rataouille");
+		String[] actors = {"Actor Name","",""};
+		movieArray.add(new Movie("The Hobbit", "Fantasy", 8, actors, new File("./LordOfTheRings.png")));
+		String[] actors2 = {"Actor Name","",""};
+		movieArray.add(new Movie("Maleficent", "Fantasy", 8, actors2, new File("./LordOfTheRings.png")));
+		String[] actors3 = {"Actor Name","",""};
+		movieArray.add(new Movie("Lord of the Rings", "Fantasy", 8, actors3, new File("./LordOfTheRings.png")));
+		String[] actors4 = {"Actor Name","",""};
+		movieArray.add(new Movie("Harry Potter", "Fantasy", 8, actors4, new File("./HarryPotter.png")));
+		String[] actors5 = {"Actor Name","",""};
+		movieArray.add(new Movie("The Martian", "Sci-Fi", 8, actors5, new File("./LordOfTheRings.png")));
+		String[] actors6 = {"Actor Name","",""};
+		movieArray.add(new Movie("Ender's Game", "Sci-Fi", 8, actors6, new File("./LordOfTheRings.png")));
+		String[] actors7 = {"Actor Name","",""};
+		movieArray.add(new Movie("The Pricess Bride", "Romance", 8, actors7, new File("./LordOfTheRings.png")));
+		String[] actors8 = {"Actor Name","",""};
+		movieArray.add(new Movie("A Goofy Movie", "Comedy", 8, actors8, new File("./LordOfTheRings.png")));
+		String[] actors9 = {"Actor Name","",""};
+		movieArray.add(new Movie("Megamind", "Comedy", 8, actors9, new File("./LordOfTheRings.png")));
+		String[] actors10 = {"Actor Name","",""};
+		movieArray.add(new Movie("Cinderella", "Romance", 8, actors10, new File("./LordOfTheRings.png")));
+		String[] actors11 = {"Actor Name","",""};
+		movieArray.add(new Movie("The Hound of the Baskerville", "Mystery", 8, actors11, new File("./LordOfTheRings.png")));
+		String[] actors12 = {"Actor Name","",""};
+		movieArray.add(new Movie("A Study in Scarlet", "Mystery", 8, actors12, new File("./LordOfTheRings.png")));
+		String[] actors13 = {"Actor Name","",""};
+		movieArray.add(new Movie("Zootopia", "Feel-Good", 8, actors13, new File("./LordOfTheRings.png")));
+		String[] actors14 = {"Actor Name","",""};
+		movieArray.add(new Movie("Rataouille", "Feel-Good", 8, actors14, new File("./LordOfTheRings.png")));
 
 		color.setVisible (true);
 	}
