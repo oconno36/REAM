360ec96f8f7c81ed027c0c2ee674bdbf8b979115
oconno36 <54715887+oconno36@users.noreply.github.com>
2020-11-16 15:12:19 -0500

    Update MovieAppInterface.java

--- MovieAppInterface.java
+++ MovieAppInterface.java
@@ -8,6 +8,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
@@ -41,6 +44,8 @@ public class MovieAppInterface extends JFrame implements ActionListener
 	JLabel movie1 = new JLabel();
 	JLabel movie2 = new JLabel();
 	private JButton searchButton;
+	private JButton sortByRating;
+	private JButton aboutButton;
 	// images
 	JLabel label;
 	JLabel label2;
@@ -64,6 +69,8 @@ public class MovieAppInterface extends JFrame implements ActionListener
 		gBut8 = new JButton("Mystery");
 		gBut9 = new JButton("Feel-Good");
 		searchButton = new JButton("Search");
+		sortByRating = new JButton("Sort by Rating");
+		aboutButton = new JButton("About");
 		gBut4.addActionListener(this);
 		gBut5.addActionListener(this);
 		gBut6.addActionListener(this);
@@ -71,6 +78,8 @@ public class MovieAppInterface extends JFrame implements ActionListener
 		gBut8.addActionListener(this);
 		gBut9.addActionListener(this);
 		searchButton.addActionListener(this);
+		sortByRating.addActionListener(this);
+		aboutButton.addActionListener(this);
 		panel = new JPanel();
 		
 		panel.add(but1);
@@ -79,40 +88,40 @@ public class MovieAppInterface extends JFrame implements ActionListener
 
 		JFrame frame = new JFrame();
 		frame.add(panel);
-		frame.setSize(350, 400);
+		frame.setSize(455, 600);
 		frame.setTitle("MovieApp");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
 		
 		String[] actors = {"Actor Name","",""};
-		movieArray.add(new Movie("The Hobbit", "Fantasy", 8, actors, new File("./TheHobbit.png")));
+		movieArray.add(new Movie("The Hobbit", "Fantasy", 10, actors, new File("./TheHobbit.png")));
 		String[] actors2 = {"Actor Name","",""};
-		movieArray.add(new Movie("Maleficent", "Fantasy", 8, actors2, new File("./Maleficent.png")));
+		movieArray.add(new Movie("Maleficent", "Fantasy", 6, actors2, new File("./Maleficent.png")));
 		String[] actors3 = {"Actor Name","",""};
-		movieArray.add(new Movie("Lord of the Rings", "Fantasy", 8, actors3, new File("./LordOfTheRings.png")));
+		movieArray.add(new Movie("Lord of the Rings", "Fantasy", 10, actors3, new File("./LordOfTheRings.png")));
 		String[] actors4 = {"Actor Name","",""};
-		movieArray.add(new Movie("Harry Potter", "Fantasy", 8, actors4, new File("./HarryPotter.png")));
+		movieArray.add(new Movie("Harry Potter", "Fantasy", 9, actors4, new File("./HarryPotter.png")));
 		String[] actors5 = {"Actor Name","",""};
-		movieArray.add(new Movie("The Martian", "Sci-Fi", 8, actors5, new File("./TheMartian.png")));
+		movieArray.add(new Movie("The Martian", "Sci-Fi", 9, actors5, new File("./TheMartian.png")));
 		String[] actors6 = {"Actor Name","",""};
 		movieArray.add(new Movie("Ender's Game", "Sci-Fi", 8, actors6, new File("./Ender'sGame.png")));
 		String[] actors7 = {"Actor Name","",""};
 		movieArray.add(new Movie("The Pricess Bride", "Romance", 8, actors7, new File("./ThePricessBride.png")));
 		String[] actors8 = {"Actor Name","",""};
-		movieArray.add(new Movie("A Goofy Movie", "Comedy", 8, actors8, new File("./AGoofyMovie.png")));
+		movieArray.add(new Movie("A Goofy Movie", "Comedy", 7, actors8, new File("./AGoofyMovie.png")));
 		String[] actors9 = {"Actor Name","",""};
-		movieArray.add(new Movie("Megamind", "Comedy", 8, actors9, new File("./Megamind.png")));
+		movieArray.add(new Movie("Megamind", "Comedy", 7, actors9, new File("./Megamind.png")));
 		String[] actors10 = {"Actor Name","",""};
-		movieArray.add(new Movie("Cinderella", "Romance", 8, actors10, new File("./Cinderella.png")));
+		movieArray.add(new Movie("Cinderella", "Romance", 6, actors10, new File("./Cinderella.png")));
 		String[] actors11 = {"Actor Name","",""};
-		movieArray.add(new Movie("The Hound of the Baskerville", "Mystery", 8, actors11, new File("./TheHoundOfTheBaskervilles.png")));
+		movieArray.add(new Movie("The Hound of the Baskerville", "Mystery", 5, actors11, new File("./TheHoundOfTheBaskervilles.png")));
 		String[] actors12 = {"Actor Name","",""};
-		movieArray.add(new Movie("A Study in Scarlet", "Mystery", 8, actors12, new File("./AStudyInScarlet.png")));
+		movieArray.add(new Movie("A Study in Scarlet", "Mystery", 4, actors12, new File("./AStudyInScarlet.png")));
 		String[] actors13 = {"Actor Name","",""};
 		movieArray.add(new Movie("Zootopia", "Feel-Good", 8, actors13, new File("./Zootopia.png")));
 		String[] actors14 = {"Actor Name","",""};
-		movieArray.add(new Movie("Ratatouille", "Feel-Good", 8, actors14, new File("./Ratatouille.png")));
+		movieArray.add(new Movie("Ratatouille", "Feel-Good", 6, actors14, new File("./Ratatouille.png")));
 	}
 
 	/**
@@ -120,27 +129,214 @@ public class MovieAppInterface extends JFrame implements ActionListener
 	 */
 	@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent event) 
-	{ 
-		if(event.getActionCommand().equals("Profile"))
-		{
-			// Removes Genres buttons
-			panel.remove(gBut4);
-			panel.remove(gBut5);
-			panel.remove(gBut6);
-			panel.remove(gBut7);
-			panel.remove(gBut8);
-			panel.remove(gBut9);
-			panel.remove(info);
-			// Removes Movie features
-			prompt.setText("Profile Information:\n");
-			panel.remove(search);
-			panel.remove(searchButton);
-			panel.remove(info);
-			moviePicUp = false;
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
+	{
+		panel.removeAll();
+		panel.add(but1);
+		panel.add(but2);
+		panel.add(but3);
+		
+		// ** Movie tab
+		if(event.getActionCommand().equals("Movies")) {
+			// Movie tab
+			panel.add(prompt);
+			// Search feature
+			panel.add(search);
+			panel.add(searchButton);
+			// Sort feature
+			panel.add(sortByRating);
+			
+			prompt.setText("This is a list of movies\n");
+			
+			// Movie list
+			try {
+				if(!moviePicUp) {
+					for(int i = 0; i < movieArray.size(); i++) {
+						BufferedImage image = ImageIO.read(movieArray.get(i).getImageFile());
+						Image scaledImage = image.getScaledInstance(50, 75, Image.SCALE_SMOOTH);
+						movieArray.get(i).movieImage = new JLabel(new ImageIcon(scaledImage));
+					}
+					moviePicUp = true;
+				}
+			} 
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			
+			for(int i = 0; i < movieArray.size(); i++) {
+				movieArray.get(i).getMovieTitle().setText(movieArray.get(i).getName());
+				panel.add(movieArray.get(i).getMovieTitle());
+				//aboutButton = new JButton("About " + i);
+				//aboutButton.addActionListener(this);
+				//panel.add(aboutButton);
+				panel.add(movieArray.get(i).getMovieImage());
+			}
+			panel.repaint();
+		}
+		
+		if(event.getActionCommand().equals("Search")) {
+			panel.add(prompt);
+			// Search feature
+			panel.add(search);
+			panel.add(searchButton);
+			// Sort feature
+			panel.add(sortByRating);
+			
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getName().contains(search.getText())) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Sort by Rating")) {
+			panel.add(prompt);
+			// Search feature
+			panel.add(search);
+			panel.add(searchButton);
+			// Sort feature
+			panel.add(sortByRating);
+			
+			// sorts copy of array list
+			ArrayList<Movie> movieSort = new ArrayList<>(movieArray);
+			// sorts with bubble sort by rating
+			for(int i = 0; i < movieSort.size() - 1; i++) {
+				for(int j = 0; j < movieSort.size() - i -1; j++) {
+					if(movieSort.get(j).getRate() > movieSort.get(j+1).getRate()) {
+						Movie temp = movieSort.get(j);
+						movieSort.set(j, movieSort.get(j+1));
+						movieSort.set(j+1, temp);
+					}
+				}
+			}
+			Collections.reverse(movieSort);
+			
+			// displays sorted movies
+			for(int i = 0; i < movieSort.size(); i++) {
+				panel.add(movieSort.get(i).getMovieTitle());
+				panel.add(movieSort.get(i).getMovieImage());
+			}
+		}
+		
+		// ** Genre tab
+		if(event.getActionCommand().equals("Genres")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+		}
+		
+		if(event.getActionCommand().equals("Fantasy")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Fantasy")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Comedy")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Comedy")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Romance")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Romance")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Sci-Fi")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Sci-Fi")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Mystery")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Mystery")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		if(event.getActionCommand().equals("Feel-Good")) {
+			panel.add(prompt);
+			prompt.setText("This is a list of genres\n");
+			panel.add(gBut4);
+			panel.add(gBut5);
+			panel.add(gBut6);
+			panel.add(gBut7);
+			panel.add(gBut8);
+			panel.add(gBut9);
+			for(int i = 0; i < movieArray.size(); i++) {
+				if(movieArray.get(i).getGenre().equals("Feel-Good")) {
+					panel.add(movieArray.get(i).getMovieTitle());
+					panel.add(movieArray.get(i).getMovieImage());
+				}
+			}
+		}
+		
+		// ** Profile tab
+		if(event.getActionCommand().equals("Profile")) {
+			panel.add(prompt);
+			prompt.setText("                               Profile Information:");
 			
 			Scanner obj = null;
 			File users = null;
@@ -171,192 +367,9 @@ public class MovieAppInterface extends JFrame implements ActionListener
 			panel.add(info);
 		}
 		
-		if(event.getActionCommand().equals("Movies")) {
-			// Removes Genres buttons
-			panel.remove(gBut4);
-			panel.remove(gBut5);
-			panel.remove(gBut6);
-			panel.remove(gBut7);
-			panel.remove(gBut8);
-			panel.remove(gBut9);
-			panel.remove(info);
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			
-			// Search feature
-			panel.add(search);
-			panel.add(searchButton);
-			
-			// Movie list
-			/*
-			panel.add(prompt);
-			prompt.setText("This is a list of movies: ");
-			movies.setText("\t \t \t");
-			
-			for(int i = 0; i < movieArray.size(); i++) {
-				movies.setText(movies.getText() + "\n" + movieArray.get(i));
-			}
-			panel.add(movies);
-			*/
-			
-			// Movie Labels with pictures
-			//panel.setLayout(new GridLayout(14, 2));
-			
-			try {
-				if(!moviePicUp) {
-					for(int i = 0; i < movieArray.size(); i++) {
-						BufferedImage image = ImageIO.read(movieArray.get(i).getImageFile());
-						Image scaledImage = image.getScaledInstance(50, 75, Image.SCALE_SMOOTH);
-						movieArray.get(i).movieImage = new JLabel(new ImageIcon(scaledImage));
-					}
-					moviePicUp = true;
-				}
-			} 
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			for(int i = 0; i < movieArray.size(); i++) {
-				movieArray.get(i).getMovieTitle().setText(movieArray.get(i).getName());
-				panel.add(movieArray.get(i).getMovieTitle());
-				panel.add(movieArray.get(i).getMovieImage());
-			}
-		}
-		
-		if(event.getActionCommand().equals("Search")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getName().contains(search.getText())) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			
-			/*
-			movies.setText("");
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).contains(search.getText())) {
-					movies.setText(movies.getText() + "\n" + movieArray.get(i));
-				}
-			}
-			movies.setText(movies.getText() + "\n\t\t\t");
-			*/
-		}
-		
-		if(event.getActionCommand().equals("Genres")) {
-			// Remove Movie features
-			prompt.setText("This is a list of genres\n");
-			panel.remove(search);
-			panel.remove(searchButton);
-			panel.remove(info);
-			moviePicUp = false;
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			
-			panel.add(prompt);
-			panel.add(gBut4);
-			panel.add(gBut5);
-			panel.add(gBut6);
-			panel.add(gBut7);
-			panel.add(gBut8);
-			panel.add(gBut9);
-		}
-		
-		if(event.getActionCommand().equals("Fantasy")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Fantasy")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("Lord of the Ring \t \t \nHarry Potter");
-			//movies.setText(movies.getText() + "\nMaleficent \nThe Hobbit");
-		}
-		
-		if(event.getActionCommand().equals("Comedy")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Comedy")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("A Goofy Movie \t \t \nMegamind");
-		}
-		
-		if(event.getActionCommand().equals("Romance")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Romance")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("The Pricess Bride \t \t \nCinderella");
-		}
-		
-		if(event.getActionCommand().equals("Sci-Fi")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Sci-Fi")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("The Martian \t \t \nEnder's Game");
-		}
-		
-		if(event.getActionCommand().equals("Mystery")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Mystery")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("The Hound of the Baskerville \t \t \nA Study in Scarlet");
-		}
-		
-		if(event.getActionCommand().equals("Feel-Good")) {
-			for(int i = 0; i < movieArray.size(); i++) {
-				panel.remove(movieArray.get(i).getMovieTitle());
-				panel.remove(movieArray.get(i).getMovieImage());
-			}
-			for(int i = 0; i < movieArray.size(); i++) {
-				if(movieArray.get(i).getGenre().equals("Feel-Good")) {
-					panel.add(movieArray.get(i).getMovieTitle());
-					panel.add(movieArray.get(i).getMovieImage());
-				}
-			}
-			//movies.setText("Zootopia \t \t \nRatatouille");
-		}
-		
-		//
+		// refresh bug fixed
+		panel.revalidate();
+		panel.repaint();
 	}
 
 	/**
